a530e2c0c10e8cf9b358f326ef1674a208454b5d
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2025-09-21 17:21:56 +0300

    Add login form handling script and script to change body background color on button click

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+  
+btn.addEventListener("click", () => {
+    const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor; 
+  span.textContent = randomColor;
+});
+  
